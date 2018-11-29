3c0202e9ad2a217c76ec609a84eb0d482ac55c99
cocostru <dk.pochtamp@gmail.com>
2018-11-29 16:01:44 +0800

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,target-densitydpi=device-dpi, user-scalable=no" />
+
+        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+
+        <link href="css/style-white.css" rel="stylesheet" type="text/css"/>
+
+        <title>Profile - Future</title>
+    </head>
+
+    <body>
+
+        <div class="wrap">
+
+            <div class="wallpaper"></div>
+
+        </div>
+
+    </body>
+
+</html>
